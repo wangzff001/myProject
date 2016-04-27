27a9313bdf0d337d28e633ae4e7aad2a4b3e8158
wangyunfeng3 <wangzff@126.com>
2016-04-27 10:40:42 +0800

    my frist test java  project

---
+++ untitled/src/MyFristClass.java
@@ -0,0 +1,19 @@
+/**
+ * Created by wangyunfeng3 on 2016/4/27.
+ */
+public class MyFristClass {
+    public String string = new String();
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public static void main(String args[]) {
+        System.out.print(333);
+    }
+
+}
